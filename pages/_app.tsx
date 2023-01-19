7fdde496b8ad480589d579a8b9a231a9c39abeb8
borisigorevich <borysigorevich@gmail.com>
2023-01-20 01:29:31 +0200

    3d cv

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Canvas } from '@react-three/fiber'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <div className='h-screen'>
+                <Canvas
+                    camera={{
+                        fov: 45,
+                        near: 1,
+                        far: 200,
+                        position: [-3, 2, 5],
+                    }}
+                >
+                    <Component {...pageProps} />
+
+                    {/*<OrbitControls makeDefault />*/}
+                </Canvas>
+            </div>
+        </>
+    )
 }
